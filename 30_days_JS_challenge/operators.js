f6ade092c7eb4ec4bdb407c0bb9e30fcbdbfa5ec
shripad03 <shripad.avhad@gmail.com>
2025-06-13 23:24:18 +0530

    day1 & 2

---
+++ 30_days_JS_challenge/operators.js
@@ -0,0 +1,68 @@
+let a = 30;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+// Arithmetic Operators
+let x = 10;
+let y = 5;
+let z = x * y;
+console.log(z);
+
+// Division
+let p = 20;
+let q = 4;
+let r = p / q;
+console.log(r);
+
+// Modulus
+let m = 10; 
+let n = 3;
+let mod = m % n;
+console.log(mod);
+
+// Increment
+let increment = 5;
+increment++;
+console.log(increment);
+
+// Decrement
+let decrement = 5;
+decrement--;
+console.log(decrement);
+
+// Exponentiation
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
+
+// Assignment Operators
+let num = 10;
+num += 5; // num = num + 5
+console.log(num);
+num -= 3; // num = num - 3
+console.log(num);
+num *= 2; // num = num * 2
+console.log(num);
+num /= 4; // num = num / 4
+console.log(num);
+num %= 3; // num = num % 3
+console.log(num);
+
+// Comparison Operators
+let a1 = 10;
+let a2 = 20;
+console.log(a1 == a2); // Equal to
+console.log(a1 != a2); // Not equal to
+console.log(a1 > a2);  // Greater than
+console.log(a1 < a2);  // Less than
+console.log(a1 >= a2); // Greater than or equal to
+console.log(a1 <= a2); // Less than or equal to
+
+// Logical Operators
+let condition1 = true;
+let condition2 = false;
+console.log(condition1 && condition2); // Logical AND
+console.log(condition1 || condition2); // Logical OR
+console.log(!condition1);               // Logical NOT
---
+++ 30_days_JS_challenge/variables.js
@@ -0,0 +1,7 @@
+var a = 30
+console.log(a);
+a = 'Hello World'
+console.log(a);
+
+var b = 30;
+console.log(b);
